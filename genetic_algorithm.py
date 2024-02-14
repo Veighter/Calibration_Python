d6773987112a9675a00dfac6b5cb6d0ac1c8e158
Veighter <59057919+Veighter@users.noreply.github.com>
2024-02-14 16:17:10 +0100

    Add module for differential evolutionary algorithm (Wikipedia)

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,24 +1,5 @@
 
-"""_summary_
-Die Python Datei ist ein differentieller evoluionaerer(genetischer) Algorithmus zur direkten Suche des globalen Maximums
-der Kostenfunktion (Euklid-Norm) für die Kalibrierung einer IMU
-"""
 
-""" 
-Erklärung/ Terminologie
-1. Individuen sind die Grundform eines gentischen Algorithmus 
-    - jedes Individuum welches existiert, existiert in einer Generation, einem Zeitschritt
-    - Individuen einer Generation bilden die Population
-2. Natuerliche Selektion
-    - die besten Individueen setzen sich durch und geben ihre Gene weiter
-    - Fitnessfunktion, meist Zielfunktion, ist das Auswahlkriterium
-3. Nachwuchs / Nächste Generation
-    - Die Erzeugung einer nächsten Generation erzeugt durch Nachwuchs aus den Zusammenkommen der vorherigen
-    - verschiedene Paarungsmöglichkeiten für Gene (Cross-Over, Zahlendreher, Ausschneiden, weitere in Vorlesung "Computational Intelligence")
-4. Mutation in der Generation bringt Vielfalt und Diversität
-    - meist zufällig
-5. Solange Selektion bis Abbruchkriterium (durch Fintessfunktion) erreicht ist
-"""
 import data_plotting as dp
 import random as rd 
 import numpy as np
@@ -172,16 +153,7 @@ def determine_static_coefficients(dataset):
 
     return static_coefficients
 
-"""
 
-
-# TODO variation (yiel offspring)
-# TODO evaluation (of offspring)
-# TODO survival selection (yields new population)
-# TODO stop
-# TODO ouput of best individual
-
-"""
 
 
 # [x] parent selection / evolution
