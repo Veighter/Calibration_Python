Inverse quaternion operator q*vq fixes the inverted scaling

calibration_wo_external_equipment.py
@@ -119,7 +119,7 @@ def optimize_acc_lm(a, static_intervals_list, thresholds):
         calibration_params = None
         avg_measurements = avg_measurements_static_interval(a, static_intervals)
         initial_parameter_vector = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0] # 12 Params for the Matrix and the bias
-        calibration_params = least_squares(acc_residuals, initial_parameter_vector, args=(avg_measurements, []), max_nfev=max_nfev, ftol=ftol, method='lm')
+        calibration_params = least_squares(acc_residuals, initial_parameter_vector, args=(avg_measurements, []), max_nfev=max_nfev, ftol=ftol, method='lm', verbose=1)
         opt_param.append((calibration_params['x'], calibration_params['cost']))
         
     costs = [cost for _, cost in opt_param]
@@ -222,61 +222,68 @@ def optimze_gyro_lm(w, static_intervals, a_O, m_O, T_init):
     b_w = np.mean(w[0:T_init*sample_rate, :], axis=0)
     w_b_free = w - b_w
 
-    a_0_avg = avg_measurements_static_interval(a_O, static_intervals)
-    m_0_avg = avg_measurements_static_interval(m_O, static_intervals)
+    a_O_avg = avg_measurements_static_interval(a_O, static_intervals)
+    m_O_avg = avg_measurements_static_interval(m_O, static_intervals)
 
     initial_parameter_vector = [1, 0, 0, 0, 1, 0, 0, 0, 1] 
-    calibration_params = least_squares(gyro_residuals, initial_parameter_vector, args=(a_0_avg, m_0_avg, w_b_free, static_intervals), max_nfev=100000, ftol=ftol, verbose=1, method='lm')
+    calibration_params = least_squares(gyro_residuals, initial_parameter_vector, args=(a_O_avg, m_O_avg, w_b_free, static_intervals), max_nfev=max_nfev, ftol=ftol, verbose=1, method='lm')
 
-    return calibration_params['x']
+    return np.append(calibration_params['x'], b_w.tolist())
 
 
 def gyro_residuals(parameter_vector, *args):
     a_O_avg, m_O_avg, w_b_free, static_intervals = args
 
-    w_uncalibrated_b_free = []
+    residuals = []
 
     for i in range(len(static_intervals)):
 
         if i > 0:
-            a_k_1 = a_O_avg[i-1]
-            m_k_1 = m_O_avg[i-1]
-            a_k = a_O_avg[i]
-            m_k = m_O_avg[i]
+            a_t_1 = a_O_avg[i-1]
+            m_t_1 = m_O_avg[i-1]
+            a_t = a_O_avg[i]
+            m_t = m_O_avg[i]
             motion_interval_bound_left = static_intervals[i-1][1]
             motion_interval_bound_right = static_intervals[i][0]
         
-        w_k_1_to_k = w_b_free[motion_interval_bound_left:motion_interval_bound_right, :]
+            w_t_1_to_t = w_b_free[motion_interval_bound_left:motion_interval_bound_right, :]
 
-        R = quaternion_rotation(parameter_vector, w_k_1_to_k)
+            q = quaternion_integration(parameter_vector, w_t_1_to_t)
 
+            v_a = quaternion.as_vector_part(q.conjugate()*quaternion.from_vector_part(a_t_1)*q)
+            v_m = quaternion.as_vector_part(q.conjugate()*quaternion.from_vector_part(m_t_1)*q)
 
-    
+            diff_a = np.linalg.norm(v_a-a_t)
+            diff_m = np.linalg.norm(v_m-m_t)
 
-    
+            residuals.append(diff_a+diff_m)
+
+    return residuals
 
 # vllt ist hier die Dimension nicht richtig!!
 def sensor_error_model_gyro_transformation(parameter_vector, w):
     Theta = np.array([parameter_vector[0:3], parameter_vector[3:6], parameter_vector[6:9]])
     return Theta@w.T
 
-def quaternion_rotation(parameter_vector, w):
-    w_bar = sensor_error_model_gyro_transformation(parameter_vector, w)
+def quaternion_integration(parameter_vector, w):
+    w_bar = sensor_error_model_gyro_transformation(parameter_vector, w).T
 
-    q_t = np,quaternion(1,0,0,0)
+    dt = 1/sample_rate
 
-    for meas in w:
-        w_x=meas[0]
-        w_y=meas[1]
-        w_z=meas[2]
+    q_t = np.quaternion(1,0,0,0)
+
+    for i in range(len(w_bar)):
+        w_x=w_bar[i,0]
+        w_y=w_bar[i,1]
+        w_z=w_bar[i,2]
+
+        w_quat = np.quaternion(0,w_x,w_y,w_z)
 
         # Equation (8) aus "Automatic Calibration IMU"
-        q_t = q_t+1./2*quaternion(-w_x*q_t.x-w_y*q_t.y-w_z*q_t.z, w_x*q_t.w+w_z*q_t.y-w_y*q_t.z, w_y*q_t.w-w_z*q_t.x+w_x*q_t.z, w_z*q_t.w+w_y*q_t.x-w_x*q_t.y)
-
+        q_t = q_t+(1./2)*w_quat*q_t*dt
+        q_t = q_t/np.linalg.norm(quaternion.as_float_array(q_t))
 
     return q_t
-
-    pass
 
  # axis_misalignment_matrix = np.array([parameter_vector[0:3], parameter_vector[3:6], parameter_vector[6:9]])
     # scaling_matrix = np.eye(3)

imu_calibration_9DOF.py
@@ -64,7 +64,7 @@ def gyro_fitness(parameter_vector, *args):
 
 def get_calibrated_measurements(raw_measurements, calibration_params, sensor):
     calibrated_measurements = []
-    if sensor == 'acc':
+    if sensor == 'acc' or sensor == 'gyro':
         theta = np.array([calibration_params[0:3], calibration_params[3:6], calibration_params[6:9]])
         bias = np.array([calibration_params[9], calibration_params[10], calibration_params[11]])
         for raw_measurement in raw_measurements:
@@ -79,11 +79,12 @@ def get_calibrated_measurements(raw_measurements, calibration_params, sensor):
     #     for raw_measurement in raw_measurements:
     #             calibrated_measurements.append((np.linalg.inv(axis_misalignment_matrix)@scaling_matrix@axis_misalignment_matrix@raw_measurement.T)-bias.T)
     if sensor == 'mag':    
-      #  Define calibration parameters
+    #  Define calibration parameters
         A = np.array(   [[0.941657, -0.005734, -0.004382],
                         [-0.005734, 0.989327, 0.011440],
                         [-0.004382, 0.011440, 0.949412]])
         b = np.array([-21.348454, 15.020109, 60.648129])
+
         
         for raw_measurement in raw_measurements:
             calibrated_measurements.append(A@(raw_measurement-b).T)
@@ -129,25 +130,55 @@ def main ():
 
 
     opt_param_acc, opt_threshold = cwee.optimize_acc_lm(acc_measurements, static_intervals_list=static_intervals, thresholds=thresholds)
-   # opt_param_mag = cwee.optimize_mag_diff_ev(mag_measurements, static_intervals[thresholds.index(opt_threshold)])
-   # opt_param_mag = cwee.optimize_mag_lm(mag_measurements, static_intervals[thresholds.index(opt_threshold)])
-    avg_measurements_opt_static_interval = np.array([cwee.avg_measurements_static_interval(mag_measurements, static_intervals[thresholds.index(opt_threshold)])])
+    #opt_param_mag = cwee.optimize_mag_diff_ev(mag_measurements, static_intervals[thresholds.index(opt_threshold)])
+    #opt_param_mag = cwee.optimize_mag_lm(mag_measurements, static_intervals[thresholds.index(opt_threshold)])
+    mag_avg_opt_static_interval = np.array([cwee.avg_measurements_static_interval(mag_measurements, static_intervals[thresholds.index(opt_threshold)])])
 
-
-    print(f"Opt_Params Accelerometer: {opt_param_acc}")
-  #  print(f"Opt_Params Magnetometer: {opt_param_mag}")
-    #print(f"Avg_measurements in Interval: {avg_measurements_opt_static_interval}")
-
-    return 0
+    acc_avg_opt_static_interval = np.array([cwee.avg_measurements_static_interval(acc_measurements, static_intervals[thresholds.index(opt_threshold)])])
 
     calibrated_acc_measurements = get_calibrated_measurements(acc_measurements, opt_param_acc, "acc")
 
-
     opt_param_mag = None
+    
     calibrated_mag_measurements = get_calibrated_measurements(mag_measurements,  opt_param_mag, 'mag')
 
+
+    print(f"Opt_Params Accelerometer: {opt_param_acc}")
+
+    opt_param_gyro = cwee.optimze_gyro_lm(gyro_measurements, static_intervals[thresholds.index(opt_threshold)],calibrated_acc_measurements, calibrated_mag_measurements, T_init=150)
+
+    print(f"Opt_Params Gyroscope: {opt_param_gyro}")
+
+    calibrated_gyro_measurements = get_calibrated_measurements(gyro_measurements, opt_param_gyro, 'gyro')
+
+    dp.plot_measurements_out_of_data([calibrated_acc_measurements, calibrated_gyro_measurements, calibrated_mag_measurements], calibrated=True)
+
+    return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #print(f"Opt_Params Magnetometer: {opt_param_mag}")
+    #print(f"Avg_measurements in Interval: {avg_measurements_opt_static_interval}")
+
+
+   
     
-    calibrated_avg_mag_measurements = get_calibrated_measurements(avg_measurements_opt_static_interval, opt_param_mag,"mag")
+    #calibrated_avg_mag_measurements = get_calibrated_measurements(mag_avg_opt_static_interval, opt_param_mag,"mag")
 
     # fig, [ax1, ax2] = plt.subplots(2,1)
     # ax1.plot(time, calibrated_acc_measurements)
@@ -166,7 +197,7 @@ def main ():
 
     # Plot XZ data
     plt.figure()
-    plt.plot(avg_measurements_opt_static_interval[:,0], avg_measurements_opt_static_interval[:,2], 'b*', label='Raw Meas.')
+    plt.plot(mag_avg_opt_static_interval[:,0], mag_avg_opt_static_interval[:,2], 'b*', label='Raw Meas.')
     plt.plot(calibrated_avg_mag_measurements[:,0], calibrated_avg_mag_measurements[:, 2], 'r*', label='Calibrated Meas.')
     plt.title('XZ Magnetometer Data')
     plt.xlabel('X [uT]')
@@ -177,7 +208,7 @@ def main ():
 
     # Plot XY data
     plt.figure()
-    plt.plot(avg_measurements_opt_static_interval[:,0], avg_measurements_opt_static_interval[:,1], 'b*', label='Raw Meas.')
+    plt.plot(mag_avg_opt_static_interval[:,0], mag_avg_opt_static_interval[:,1], 'b*', label='Raw Meas.')
     plt.plot(calibrated_avg_mag_measurements[:,0], calibrated_avg_mag_measurements[:, 1], 'r*', label='Calibrated Meas.')
     plt.title('XY Magnetometer Data')
     plt.xlabel('X [uT]')
@@ -188,7 +219,7 @@ def main ():
 
     # Plot YZ data
     plt.figure()
-    plt.plot(avg_measurements_opt_static_interval[:,1], avg_measurements_opt_static_interval[:,2], 'b*', label='Raw Meas.')
+    plt.plot(mag_avg_opt_static_interval[:,1], mag_avg_opt_static_interval[:,2], 'b*', label='Raw Meas.')
     plt.plot(calibrated_avg_mag_measurements[:,1], calibrated_avg_mag_measurements[:, 2], 'r*', label='Calibrated Meas.')
     plt.title('YZ Magnetometer Data')
     plt.xlabel('Y [uT]')
@@ -205,7 +236,7 @@ def main ():
 
     ## Natuerlich nur die Punkte nutzen, welche geaveraget wurden!!!
     # Das hier ist sinnlos
-    for avg_measurement in avg_measurements_opt_static_interval:
+    for avg_measurement in mag_avg_opt_static_interval:
 
         mag_x = avg_measurement[0]
         mag_y = avg_measurement[1]